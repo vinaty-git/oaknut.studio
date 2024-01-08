625a556b009a74bdbad480ac70ad99a96a2ea983
kirill <kirill.labutkin@gmail.com>
2024-01-08 14:00:50 +0100

    0.0.1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="oaknut-logo" alt="logo" />
+        <p className='company-name'>
+          Oaknut Studio 1
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
